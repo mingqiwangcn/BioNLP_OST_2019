d027077dd95d07071f3caefa73c7f04a8659158a
Qiming Wang <mingqiwangcn@gmail.com>
2020-09-06 19:13:08 -0500

    add span annotation

--- merge_qas.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for file_path in **/*_questions.txt; do
-    cat ${file_path} >> bionlp_questions.txt
-done
-for file_path in **/*_answers.txt; do
-    cat ${file_path} >> bionlp_answers.txt
-done
