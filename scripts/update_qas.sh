4a25c2d3ee454ae889c436f027a4aa80751496e3
Qiming Wang <mingqiwangcn@gmail.com>
2020-09-14 14:13:17 -0500

    add question info and sample qids

--- scripts/update_qas.sh
+++ scripts/update_qas.sh
@@ -1,1 +1,1 @@
-python ~/code/fabric-qa/text_to_table/data_process/BioNLP_OST_2019/gen_qas.py --output_dir qas
+python ~/code/fabric-qa/text_to_table/data_process/BioNLP_OST_2019/gen_qas.py --output_dir ~/data/BioNLP_OST_2019/qas
